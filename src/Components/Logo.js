368967f2df995382b41992ebeea5145a91f87d38
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2022-09-06 17:06:33 -0400

    added tilt

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Tilt from "react-parallax-tilt";
 
 const Logo = () => {
   return (
-    <div className="ma4 mt0">
-      <p>Logo</p>
+    <div className="ma4 mt0 ct">
+      <Tilt style={{ height: "150", width: "150", background: "red" }}>
+        <div className="Tilt-inner">some text</div>
+        <p>Logo</p>
+      </Tilt>
     </div>
   );
 };
