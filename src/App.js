a4e8e106988666a60155fee70cf3e41131c3d8dd
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2022-09-06 16:47:21 -0400

    up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import Nav from "./Components/Nav";
+import Logo from "./Components/Logo";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Nav />
+        <Logo />
+        {/* <ImageLinkForm/>
+        <FaceRecogn /> */}
       </div>
     );
   }
