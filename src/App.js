9e87f562a015af8693012fa018318d0362246087
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2022-09-06 21:33:25 -0400

    added particles

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Nav from "./Components/Nav";
 import Logo from "./Components/Logo/Logo";
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
 import Rank from "./Components/Rank/Rank";
+import ParticlesMaker from "./Components/ParticlesMaker/ParticlesMaker";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ParticlesMaker />
         <Nav />
         <Logo />
         <Rank />
